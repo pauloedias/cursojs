3beaa00aace5d40838bb5afdf11ad80b659f3adb
Paulo Eduardo Dias <pauloe.jba@gmail.com>
2024-08-16 12:49:06 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+/*
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+do {
+    console.log(`Passo ${c}!`)
+    c++
+} while(c <=500)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
